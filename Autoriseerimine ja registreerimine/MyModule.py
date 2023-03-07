5126f2b60b42cf6fca8cbcd1665cf5b38eb3d192
lehja <opilane@HP-CZC2349HTZ>
2023-03-07 12:28:01 +0200

    obhyu

---
+++ Autoriseerimine ja registreerimine/MyModule.py
@@ -0,0 +1,97 @@
+﻿import string
+from random import choice
+
+# luua tühjad nimekirjad login ja paroolid
+# создавать пустые списки логинов и паролей
+logins = []
+passwords = []
+
+# funktsioon juhusliku parooli genereerimiseks
+# функция генерации случайного пароля
+def salasona(k: int):
+  sala=""
+  for i in range(k):
+   t=choice(string.ascii_letters) #Aa...Zz
+   num=choice([1,2,3,4,5,6,7,8,9,0])
+   t_num=[t,str(num)]
+   sala+=choice(t_num)
+  return sala
+
+# funktsioon kasutaja registreerimiseks
+# функция регистрации пользователя
+def register():
+    login = input("Sisesta oma login: ")
+    if login in logins:
+        print("See login on juba votud.")
+        return
+    salasona_valik = input("Kas sa tahad juhuslik salasone? (Y/N): ")
+    if salasona_valik.lower() == 'y':
+        password = salasona(8)
+        print(f"Sinu salasona: {password}")
+    else:
+        while True:
+            password = input("Sisesta oma salasona: ")
+            if any(char.isdigit() for char in password) and any(char.islower() for char in password) and any(char.isupper() for char in password) and any(char in string.punctuation for char in password):
+                break
+            else:
+                print("Teie parool peab sisaldama vähemalt ühte numbrit, ühte väiketähte, ühte suurtähte ja ühte erilist sümbolit.")
+    logins.append(login)
+    passwords.append(password)
+    print("Registreerimine õnnetus!")
+
+# funktsioon kasutaja autoriseerimiseks
+# функция авторизации пользователя
+def authorize():
+    login = input("Sisesta oma login: ")
+    if login not in logins:
+        print("See logini pole registreeritud.")
+        return
+    password = input("Sisesta oma salasona: ")
+    if password != passwords[logins.index(login)]:
+        print("Vale salasona.")
+        return
+    print("Login õnnetus!")
+
+# funktsioon nime või parooli muutmiseks
+# функция смены имени или пароля
+def change():
+    login = input("Sisesta oma login: ")
+    if login not in logins:
+        print("See nimi ei ole registreeritud.")
+        return
+    valik = input("Kas soovite muuta oma nime või parooli? (login/password): ")
+    if valik.lower() == 'login':
+        new_login = input("Sisesta uue login: ")
+        if new_login in logins:
+            print("See login on juba võtud.")
+            return
+        logins[logins.index(login)] = new_login
+        print("Login muudatus õnnetus.")
+    elif valik.lower() == 'password':
+        while True:
+            new_password = input("Sisesta uue salasone: ")
+            if any(char.isdigit() for char in new_password) and any(char.islower() for char in new_password) and any(char.isupper() for char in new_password) and any(char in string.punctuation for char in new_password):
+                break
+            else:
+                print("Teie parool peab sisaldama vähemalt ühte numbrit, ühte väiketähte, ühte suurtähte ja ühte erilist sümbolit.")
+        passwords[logins.index(login)] = new_password
+        print("Salasone muudatus õnnetus.")
+    else:
+        print("Viga.")
+
+# funktsioon unustatud parooli taastamiseks
+# функция восстановления забытого пароля
+def forgotpassword():
+    login = input("Наберите свой логин: ")
+    if login not in logins:
+        print("Вы не зарегистрированы.")
+        return
+    new_password = salasona(8)
+    passwords[logins.index(login)] = new_password
+    print(f"Sinu uus parool on: {new_password}")
+
+# funktsioon välja logimiseks
+# функция выхода из системы
+def logout():
+    print("Sa logisid välja.")
+    
---
+++ Autoriseerimine ja registreerimine/autoriseerimine.py
@@ -0,0 +1,23 @@
+﻿from MyModule import register, authorize, change, forgotpassword, logout
+
+while True:
+    print("Valige:")
+    print("1. Registreerima")
+    print("2. Logi sisse")
+    print("3. Muudata login voi salasõna")
+    print("4. Unustasid salasone")
+    print("5. Logi välja")
+    valik = input("Sisesta number (1-5): ")
+    if valik == '1':
+        register()
+    elif valik == '2':
+        authorize()
+    elif valik == '3':
+        change()
+    elif valik == '4':
+        forgotpassword()
+    elif valik == '5':
+        logout()
+        exit()
+    else:
+        print("Viga! Palun andke number 1st kuni 5ni.")
